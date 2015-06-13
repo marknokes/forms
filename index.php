5f89801c4fff1c02ca67e15ef0b41ad6b05b750f
Mark Nokes <mark.nokes@hotmail.com>
2015-06-13 09:48:34 -0500

    cleaned up markup on index

--- index.php
+++ index.php
@@ -1,195 +1,191 @@
+<?php
+    
+require_once 'Forms/loader.php';
+
+$Form = new \Form\Form();
+
+/**
+*  Use this for an LDAP auth form
+*  $Form = new \Form\LDAP_Auth();
+*/
+
+$Form->options = array(
+    'tmp_dir'               => 'C:\\tmp\\', // required for sending email attachments. default is C:\tmp\
+    'id'                    => 'form-css-id', // required. default is form-css-id
+    'email'                 => true, // optional. default is true
+    'to'                    => 'address@domain.com', // required if email set to true above
+    'default_from'          => 'address@domain.com', // optional. if there's an email field in the form with replyTo = true, the form submitter's email will be used
+    'default_from_name'     => 'Name', // optional
+    'subject'               => 'Email Subject', // required if email set to true above
+    'stylesheet_path'       => 'styles.css', // optional. use this option to override bootstrap styles
+    'recaptcha'             => false, // required. default is false
+    'recaptcha_site_key'    => '', // optional. required if recaptcha = true
+    'recaptcha_secret_key'  => '', // optional. required if recaptcha = true
+
+    /**
+    * Use these options if using $Form = new \Form\LDAP_Auth();
+    *
+    * 'ldap_auth' => array(
+    *     'ad_server'   => 'ldap.server.hostname',
+    *     'base_dn'     => 'DC=dn,DC=base',
+    *     'ldap_user'   => 'CN=name,CN=distinguished,DC=user,DC=ldap',
+    *     'ldap_pass'   => 'ldap_user_password',
+    *     'search'      => array(
+    *         'memberOf=CN=,OU=,OU=',
+    *         'memberOf=CN=,OU=,OU=',
+    *     ),
+    *     'success'     => array(
+    *         'action'  => 'Redirect',
+    *         'data'    => 'http://www.domain.com'
+    *     )
+    * ),
+    */
+    
+    /**
+    * Use these options to save data to Hannon Hill's Cascade Server
+    *
+    * 'save'                  => array(
+    *     'to_save' => array( 
+    *         'path'     => '/path/to/folder',
+    *         'fileName' => 'filename.csv'
+    *     ),
+    *     'site' => 'Cascade Server Website Name'
+    * ),
+    * 'soapURL'               => '', // optional
+    * 'auth'                  => array( // optional
+    *     'username' => '',
+    *     'password' => ''
+    * ),
+    */
+);
+
+$Form->fields = array(
+
+    'ajax_action' => array( // required. value is either process_form or ldap_auth
+        'fieldName'     => 'ajax_action',
+        'type'          => 'hidden',
+        'default_value' => 'process_form'
+    ),
+
+    /**
+    * Use these fields if ajax_action is set to ldap_auth. Right now it only does a redirect, however
+    * you may have it save a cookie, set a session var, or something else.
+    *
+    *   'username' => array(
+    *       'fieldName' => 'Username',
+    *       'type'      => 'text'
+    *   ),
+    *   'password' => array(
+    *       'fieldName' => 'Password',
+    *       'type'      => 'password'
+    *   ),
+    */
+
+    /**
+    * Field type options
+    */
+    
+    'field-0' => array(
+        'fieldName'             => 'Email',
+        'type'                  => 'email',
+        'required'              => true,
+        'cloneable'             => false,
+        'send_email_attachment' => false,
+        'replyTo'               => true
+    ),
+    'field-1' => array(
+        'fieldName' => 'Text',
+        'type'      => 'text',
+        'required'  => false,
+        'cloneable' => false
+    ),
+    'field-2' => array(
+        'fieldName' => 'Cloneable',
+        'type'      => 'text',
+        'required'  => false,
+        'cloneable' => true
+    ),
+    'field-3' => array(
+        'fieldName'     => 'WYSIWYG',
+        'type'          => 'message',
+        'default_value' => '<h1>Heading</h1><p>Here\'s a message to be displayed on the form. This can be before or after any field.</p>'
+    ),
+    'field-4' => array(
+        'fieldName' => 'Textarea',
+        'type'      => 'textarea',
+        'required'  => false
+    ),
+    'field-5' => array(
+        'fieldName'     => 'Checkboxes',
+        'type'          => 'checkbox',
+        'required'      => false,
+        'default_value' => 'Checked',
+        'options'       => array(
+            'Value One',
+            'Value Two'
+        )
+    ),
+    'field-6' => array(
+        'fieldName' => 'Radio Buttons',
+        'type'      => 'radio',
+        'required'  => false,
+        'options'   => array(
+            'Option 1',
+            'Option 2',
+            'Option 3'
+        )
+    ),
+    'field-7' => array(
+        'fieldName' => 'Datepicker',
+        'type'      => 'datepicker',
+        'required'  => false
+    ),
+    'field-8' => array(
+        'fieldName' => 'Timepicker',
+        'type'      => 'timepicker',
+        'required'  => false
+    ),
+    'field-9' => array(
+        'fieldName' => 'Select',
+        'type'      => 'dropdown',
+        'required'  => false,
+        'options'   => array(
+            'Value One',
+            'Value Two',
+            'Value Three'
+        )
+    ),
+    'field-10' => array(
+        'fieldName' => 'Multi Select',
+        'type'      => 'dropdown',
+        'multiple'  => true,
+        'required'  => false,
+        'options'   => array(
+            'Value One',
+            'Value Two',
+            'Value Three',
+            'Value Four'
+        )
+    )
+);
+
+$Form->init();
+?>
+
 <html>
 
-    <head>
+	<head>
 
-        <title>Test Form</title>
-        
-        <meta content="text/html;charset=UTF-8" http-equiv="Content-type"/>
-        
-        <?php
-    
-        require_once 'Forms/loader.php';
+		<?php $Form->print_styles(); ?>
 
-        $Form = new \Form\Form();
+	</head>
 
-        /**
-        *  Use this for an auth form
-        *  $Form = new \Form\LDAP_Auth();
-        */
+	<body>
 
-        $Form->options = array(
-            'tmp_dir'               => 'C:\\tmp\\', // required for sending email attachments. default is C:\tmp\
-            'id'                    => 'form-css-id', // required. default is form-css-id
-            'email'                 => true, // optional. default is true
-            'to'                    => 'address@domain.com', // required if email set to true above
-            'default_from'          => 'address@domain.com', // optional. if there's an email field in the form with replyTo = true, the form submitter's email will be used
-            'default_from_name'     => 'Name', // optional
-            'subject'               => 'Email Subject', // required if email set to true above
-            'stylesheet_path'       => 'styles.css', // optional. use this option to override bootstrap styles
-            'recaptcha'             => false, // required. default is false
-            'recaptcha_site_key'    => '', // optional. required if recaptcha = true
-            'recaptcha_secret_key'  => '', // optional. required if recaptcha = true
+		<?php $Form->get(); ?>
 
-            /**
-            * Use these options if using $Form = new \Form\LDAP_Auth();
-            *
-            * 'ldap_auth' => array(
-            *     'ad_server'   => 'ldap.server.hostname',
-            *     'base_dn'     => 'DC=dn,DC=base',
-            *     'ldap_user'   => 'CN=name,CN=distinguished,DC=user,DC=ldap',
-            *     'ldap_pass'   => 'ldap_user_password',
-            *     'search'      => array(
-            *         'memberOf=CN=,OU=,OU=',
-            *         'memberOf=CN=,OU=,OU=',
-            *     ),
-            *     'success'     => array(
-            *         'action'  => 'Redirect',
-            *         'data'    => 'http://www.domain.com'
-            *     )
-            * ),
-            */
-            
-            /**
-            * Use these options to save data to Hannon Hill's Cascade Server
-            *
-            * 'save'                  => array(
-            *     'to_save' => array( 
-            *         'path'     => '/path/to/folder',
-            *         'fileName' => 'filename.csv'
-            *     ),
-            *     'site' => 'Cascade Server Website Name'
-            * ),
-            * 'soapURL'               => '', // optional
-            * 'auth'                  => array( // optional
-            *     'username' => '',
-            *     'password' => ''
-            * ),
-            */
-        );
+	</body>
 
-        $Form->fields = array(
-
-            'ajax_action' => array( // required. value is either process_form or ldap_auth
-                'fieldName'     => 'ajax_action',
-                'type'          => 'hidden',
-                'default_value' => 'process_form'
-            ),
-
-            /**
-            * Use these fields if ajax_action is set to ldap_auth. Right now it only does a redirect, however
-            * you may have it save a cookie, set a session var, or something else.
-            *
-            *   'username' => array(
-            *       'fieldName' => 'Username',
-            *       'type'      => 'text'
-            *   ),
-            *   'password' => array(
-            *       'fieldName' => 'Password',
-            *       'type'      => 'password'
-            *   ),
-            */
-
-            /**
-            * Field type options
-            */
-            
-            'field-0' => array(
-                'fieldName'             => 'Email',
-                'type'                  => 'email',
-                'required'              => true,
-                'cloneable'             => false,
-                'send_email_attachment' => false,
-                'replyTo'               => true
-            ),
-            'field-1' => array(
-                'fieldName' => 'Text',
-                'type'      => 'text',
-                'required'  => false,
-                'cloneable' => false
-            ),
-            'field-2' => array(
-                'fieldName' => 'Cloneable',
-                'type'      => 'text',
-                'required'  => false,
-                'cloneable' => true
-            ),
-            'field-3' => array(
-                'fieldName'     => 'WYSIWYG',
-                'type'          => 'message',
-                'default_value' => '<h1>Heading</h1><p>Here\'s a message to be displayed on the form. This can be before or after any field.</p>'
-            ),
-            'field-4' => array(
-                'fieldName' => 'Textarea',
-                'type'      => 'textarea',
-                'required'  => false
-            ),
-            'field-5' => array(
-                'fieldName'     => 'Checkboxes',
-                'type'          => 'checkbox',
-                'required'      => false,
-                'default_value' => 'Checked',
-                'options'       => array(
-                    'Value One',
-                    'Value Two'
-                )
-            ),
-            'field-6' => array(
-                'fieldName' => 'Radio Buttons',
-                'type'      => 'radio',
-                'required'  => false,
-                'options'   => array(
-                    'Option 1',
-                    'Option 2',
-                    'Option 3'
-                )
-            ),
-            'field-7' => array(
-                'fieldName' => 'Datepicker',
-                'type'      => 'datepicker',
-                'required'  => false
-            ),
-            'field-8' => array(
-                'fieldName' => 'Timepicker',
-                'type'      => 'timepicker',
-                'required'  => false
-            ),
-            'field-9' => array(
-                'fieldName' => 'Select',
-                'type'      => 'dropdown',
-                'required'  => false,
-                'options'   => array(
-                    'Value One',
-                    'Value Two',
-                    'Value Three'
-                )
-            ),
-            'field-10' => array(
-                'fieldName' => 'Multi Select',
-                'type'      => 'dropdown',
-                'multiple'  => true,
-                'required'  => false,
-                'options'   => array(
-                    'Value One',
-                    'Value Two',
-                    'Value Three',
-                    'Value Four'
-                )
-            )
-        );
-
-        $Form->init();
-    ?>
-    
-    <?php $Form->print_styles(); ?>
-    
-    </head>
-    
-    <body>
-        
-        <?php $Form->get(); ?>
-    
-    </body>
-
-    <?php $Form->print_scripts(); ?>
+	<?php $Form->print_scripts(); ?>
 
 </html>
